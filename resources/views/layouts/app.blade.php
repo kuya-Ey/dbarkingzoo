48763ee1420ae1ab23aa550aed3c79f5fad466ea
kuya-Ey <138701056+kuya-Ey@users.noreply.github.com>
2023-10-28 05:40:32 +0800

    commit 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,10 +85,10 @@
         <main class="py-4">
             @yield('content')
         </main>
-        Explain
+
 <!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
+  Enter
 </button>
 
 <!-- Modal -->
--- storage/framework/views/7486a4663d01cab06c9de8fa4758797e6c24071f.php
+++ storage/framework/views/7486a4663d01cab06c9de8fa4758797e6c24071f.php
@@ -26,7 +26,8 @@
     <link href="<?php echo e(asset('css/styles.css')); ?>" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
+    <div class="container">
+        <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="<?php echo e(url('/')); ?>">
@@ -87,8 +88,32 @@
         <main class="py-4">
             <?php echo $__env->yieldContent('content'); ?>
         </main>
-    </div>
 
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Enter
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+        </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src=<?php echo e(url('js/app.js')); ?>></script>
 </body>
