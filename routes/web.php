1439f4989d8722708b0a40f0adb1f02b7504b781
kuya-Ey <138701056+kuya-Ey@users.noreply.github.com>
2023-10-27 23:06:31 +0800

    added routes

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,42 @@ Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
 
+// routes for product Controller
+Route::get('/products',[ProductController::class, 'index'])->middleware('verified');
+Route::get('/product/{id}',[ProductController::class, 'show'])->middleware('verified');
+Route::get('/product',[ProductController::class, 'create'])->middleware('verified');
+Route::get('/product/{id}/edit',[ProductController::class, 'edit'])->middleware('verified');
+
+Route::post('/product',[ProductController::class, 'store'])->middleware('verified');
+Route::put('/product/{id}/edit',[ProductController::class, 'update'])->middleware('verified');
+Route::delete('/product/{id}',[ProductController::class, 'destroy'])->middleware('verified');
+
+// routes for consultation Controller
+Route::get('/consultations',[ProductController::class, 'index'])->middleware('verified');
+Route::get('/consultation/{id}',[ProductController::class, 'show'])->middleware('verified');
+Route::get('/consultation',[ProductController::class, 'create'])->middleware('verified');
+Route::get('/consultation/{id}/edit',[ProductController::class, 'edit'])->middleware('verified');
+
+Route::post('/consultation',[ProductController::class, 'store'])->middleware('verified');
+Route::put('/consultation/{id}/edit',[ProductController::class, 'update'])->middleware('verified');
+Route::delete('/consultation/{id}',[ProductController::class, 'destroy'])->middleware('verified');
+
+// routes for grooming_service Controller
+Route::get('/grooming_services',[ProductController::class, 'index'])->middleware('verified');
+Route::get('/grooming_service/{id}',[ProductController::class, 'show'])->middleware('verified');
+Route::get('/grooming_service',[ProductController::class, 'create'])->middleware('verified');
+Route::get('/grooming_service/{id}/edit',[ProductController::class, 'edit'])->middleware('verified');
+
+Route::post('/grooming_service',[ProductController::class, 'store'])->middleware('verified');
+Route::put('/grooming_service/{id}/edit',[ProductController::class, 'update'])->middleware('verified');
+Route::delete('/grooming_service/{id}',[ProductController::class, 'destroy'])->middleware('verified');
+
+// routes for pet_boarding Controller
+Route::get('/pet_boardings',[ProductController::class, 'index'])->middleware('verified');
+Route::get('/pet_boarding/{id}',[ProductController::class, 'show'])->middleware('verified');
+Route::get('/pet_boarding',[ProductController::class, 'create'])->middleware('verified');
+Route::get('/pet_boarding/{id}/edit',[ProductController::class, 'edit'])->middleware('verified');
+
+Route::post('/pet_boarding',[ProductController::class, 'store'])->middleware('verified');
+Route::put('/pet_boarding/{id}/edit',[ProductController::class, 'update'])->middleware('verified');
+Route::delete('/pet_boarding/{id}',[ProductController::class, 'destroy'])->middleware('verified');
